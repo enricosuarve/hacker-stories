fff477ee11f16f190bd01de1ecfec6f61f660a9f
enricosuarve <HLKjh*&&*^&*fhjvjhcjhFDf^%&>
2022-08-28 12:06:36 +0100

    Got to P.31

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+
+const welcome = {
+    greeting: 'Hey',
+    title: 'React',
+};
+
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+        test: "wwwww"
+    },
+    {
+        title: 'Redux',
+        url: 'https://reduc.js.org',
+        author: 'Dan Abrmov, Andrew Clarke',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
+function getTitle(title) {
+    return title;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>
+                {welcome.greeting} {welcome.title}<br/>
+                hello {getTitle('React')}
+            </h1>
+            <label htmlFor={"search"}>Search: </label>
+            <input id="search" type={"text"}/>
+            <ul>
+                {list.map(function (item) {
+                    return  (
+                        <li key={item.objectID} id={item.test}>
+                            {item.title}
+                        <ul>
+                            <li>{item.url}</li>
+                        </ul>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
