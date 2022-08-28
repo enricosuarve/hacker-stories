e54a79349c35e138c26245b129f1efccad77b84d
enricosuarve <HLKjh*&&*^&*fhjvjhcjhFDf^%&>
2022-08-28 13:30:40 +0100

    Got to P.50 - using props

--- src/App.js
+++ src/App.js
@@ -5,56 +5,63 @@ const welcome = {
     title: 'React',
 };
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-        test: "wwwww"
-    },
-    {
-        title: 'Redux',
-        url: 'https://reduc.js.org',
-        author: 'Dan Abrmov, Andrew Clarke',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
 
 const getTitle = (title) => {
     return title;
 }
 
-const App = () => (
-    <div>
-        <h1>
-            {welcome.greeting} {welcome.title}<br/>
-            Hello {getTitle('React')}
-        </h1>
-        <Search/>
-        <List/>
-    </div>
-);
+const App = () => {
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+            test: "wwwww"
+        },
+        {
+            title: 'Redux',
+            url: 'https://reduc.js.org',
+            author: 'Dan Abrmov, Andrew Clarke',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
+
+    return(
+        <div>
+            <h1>
+                {welcome.greeting} {welcome.title}<br/>
+                Hello {getTitle('React')}
+            </h1>
+            <Search/>
+            <List list={stories} extraline={"test"}/>
+        </div>
+    );
+}
 
 
-const List = () => (
+const List = (props) => (
     <ul>
-        {list.map((item) => (
-            <li key={item.objectID} id={item.test}>
-                {item.title}
-                <ul>
-                    <li>{item.url}</li>
-                </ul>
-            </li>
-        ))}
+        {props.list.map((item) => (
+        <Item key={item.objectID} item={item} extraline={" test"}/>
+            ))}
     </ul>
 );
 
+const Item =(props)=>(
+    <li key={props.item.objectID} id={props.item.test}>
+        {props.item.title}
+        &nbsp;{props.extraline}
+        <ul>
+            <li>{props.item.url}</li>
+        </ul>
+    </li>
 
+)
 const Search = () => {
     const handleChange = (event)=>{
         //synthetic event
