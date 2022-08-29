9c91b970f7d88b03e3c1473eea164425370f4a40
enricosuarve <HLKjh*&&*^&*fhjvjhcjhFDf^%&>
2022-08-29 20:38:40 +0100

    got to p.121 - NOT WORKING - used API to pull data - need to compare to code online as seems to pull the data OK (can see the objects in console.logs) but does not display on screen

--- src/App.js
+++ src/App.js
@@ -5,35 +5,7 @@ const welcome = {
     title: 'React',
 };
 
-const initialStories = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-        test: "test extra prop"
-    },
-    {
-        title: 'Redux',
-        url: 'https://reduc.js.org',
-        author: 'Dan Abrmov, Andrew Clarke',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
-
-
-const getAsyncStories = () =>
-    // new Promise((resolve,reject)=>setTimeout(reject,2000));
-    new Promise((resolve) =>
-        setTimeout(
-            () => resolve({data: {stories: initialStories}}),
-            2000
-        )
-    );
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 
 const getTitle = (title) => {
@@ -103,13 +75,14 @@ const App = () => {
 
 
     React.useEffect(() => {
-        dispatchStories({type: 'STORIES_FETCH_INIT'})
+        dispatchStories({type: 'STORIES_FETCH_INIT'});
 
-        getAsyncStories()
+        fetch(`${API_ENDPOINT}react`) //B
+            .then((response) => response.json())
             .then(result => {
                 dispatchStories({
                     type: 'STORIES_FETCH_SUCCESS',
-                    payload: result.data.stories,
+                    payload: result.hits,
                 });
             })
             .catch(() =>
